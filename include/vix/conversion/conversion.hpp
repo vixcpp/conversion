c546f4bba00b296faa6918e8cd97b4989416bd02
Gaspard Kirira <gaspardkirira9@gmail.com>
2026-04-18 16:35:18 +0300

    feat(conversion): add umbrella header

---
+++ include/vix/conversion/conversion.hpp
@@ -0,0 +1,36 @@
+/**
+ *
+ *  @file conversion/conversion.hpp
+ *  @author Gaspard Kirira
+ *
+ *  @brief Internal aggregation header for the Vix conversion module.
+ *
+ *  This file includes the core conversion utilities provided by Vix,
+ *  including parsing helpers, expected-style results, and type
+ *  conversions for integers, floats, booleans, enums, and strings.
+ *
+ *  For most use cases, prefer:
+ *    #include <vix/conversion.hpp>
+ *
+ *  Copyright 2025, Gaspard Kirira.
+ *  All rights reserved.
+ *  https://github.com/vixcpp/vix
+ *
+ *  Use of this source code is governed by a MIT license
+ *  that can be found in the License file.
+ *
+ *  Vix.cpp
+ */
+#ifndef VIX_CONVERSION_CONVERSION_HPP
+#define VIX_CONVERSION_CONVERSION_HPP
+
+#include <vix/conversion/ConversionError.hpp>
+#include <vix/conversion/Expected.hpp>
+#include <vix/conversion/Parse.hpp>
+#include <vix/conversion/ToBool.hpp>
+#include <vix/conversion/ToEnum.hpp>
+#include <vix/conversion/ToFloat.hpp>
+#include <vix/conversion/ToInt.hpp>
+#include <vix/conversion/ToString.hpp>
+
+#endif // VIX_CONVERSION_CONVERSION_HPP
